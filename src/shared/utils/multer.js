e8c0da0d64303d0d48b31f7d2887352e48c8fe43
HectorOdato <leoodato@gmail.com>
2025-11-10 20:29:34 -0300

    agregue carpeta upload para guardado de archivos

--- src/shared/utils/multer.js
+++ src/shared/utils/multer.js
@@ -2,7 +2,7 @@ import path from "path";
 import multer from "multer";
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, 'src/product/uploads/'),
+  destination: (req, file, cb) => cb(null, 'src/product/uploads/images/'),
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
     cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
