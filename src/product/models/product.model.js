862ccc986d9b724518c18cc5c503231cbe615e9e
HectorOdato <leoodato@gmail.com>
2025-11-10 19:57:45 -0300

    agregue mas atributos para crear productos al model y controller

--- src/product/models/product.model.js
+++ src/product/models/product.model.js
@@ -2,11 +2,15 @@ import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  description: { type: String, required: true },
   price: { type: Number, required: true, min: 0 },
-  stock: { type: Number, default: 0 },
-  status: { type: Boolean, default: true }
-})
+  category: { type: String, required: true },       
+  stock: { type: Number , required: true, min: 0 },
+  image: { type: String, required: true },
+  status: { type: Boolean, default: true },      
+});
 
-const Product = mongoose.model("Product", productSchema)
+const Product = mongoose.model("Product", productSchema);
 
-export default Product
+export default Product;
+
