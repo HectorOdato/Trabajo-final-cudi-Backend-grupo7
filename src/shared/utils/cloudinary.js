ad27d51d1684257ebea7c49d4fad7c8f7ef1847f
HectorOdato <leoodato@gmail.com>
2025-11-18 20:28:48 -0300

    cofiguracion de multer y cloudinary para el guardado de imagenes en el back y posteriormente al servidor en la nube

--- src/shared/utils/cloudinary.js
+++ src/shared/utils/cloudinary.js
@@ -1,26 +1,15 @@
 import { v2 as cloudinary } from "cloudinary";
-import fs from 'fs';
-console.log("ENV CLOUDINARY KEY:", process.env.CLOUDINARY_API_KEY);
-
-
-cloudinary.config({
-    
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-});
 
 export const subirImagenCloudinary = async (filePath) => {
+    const fs = await import('fs'); 
     try {
         const result = await cloudinary.uploader.upload(filePath, {
             folder: "productsEcommerseUTN" 
         });
-        
-        fs.unlinkSync(filePath); 
-
+        fs.default.unlinkSync(filePath); 
         return result; 
     } catch (error) {
-        fs.unlinkSync(filePath); 
+        fs.default.unlinkSync(filePath); 
         console.error("Error al subir a Cloudinary:", error);
         throw new Error("Fallo al subir la imagen a Cloudinary");
     }
--- src/shared/utils/multer.js
+++ src/shared/utils/multer.js
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'src/product/uploads/images'); 
+    cb(null, 'uploads/'); 
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
