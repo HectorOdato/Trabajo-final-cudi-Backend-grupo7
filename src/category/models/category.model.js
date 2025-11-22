00187095e61f306d8d46cfbd13d8fcf85710e0e4
HectorOdato <leoodato@gmail.com>
2025-11-22 15:57:04 -0300

    agregue nuevo atributo para creacion de categorias Image

--- src/category/models/category.model.js
+++ src/category/models/category.model.js
@@ -3,6 +3,7 @@ import { model, Schema } from "mongoose";
 const categorySchema = new Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
+  image:{ type: String},
   status: { type: Boolean, default: true },
 })
 
